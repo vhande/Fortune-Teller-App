45b69f975b94a4d1788cddff1ecd65852947e6ee
Hande <hande@Handes-MacBook-Air.local>
2022-10-11 21:06:34 +0200

    update

--- src/ModalComponent1.js
+++ src/ModalComponent1.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal, Button, ButtonGroup } from 'react-bootstrap'
+import { Modal, Button, ButtonGroup, Spinner } from 'react-bootstrap'
 
 function ModalComponent1({ husband, show, closeModal, location, nestedModal, fetchMalePhoto }) {
 
@@ -10,14 +10,16 @@ function ModalComponent1({ husband, show, closeModal, location, nestedModal, fet
       aria-labelledby="contained-modal-title-vcenter"
       centered>
       <Modal.Body>
-        <h4>I see...</h4>
+        {husband.age !== undefined ? <h4>I see...</h4> : "" }
         {location !== "" && husband.age !== undefined ?
           `His name is ${husband.age.name}. ` : ""}
         {husband.age !== undefined && location !== "" ?
           `He is ${husband.age.age} years old. ` : ""}
         {husband.age !== undefined && location !== "" ?
           `He is waiting for you in ${location}.` :
-          "This may take a while..." }
+          <div className="d-flex">
+            <Spinner className="spinner mx-2" animation="border" role="status">
+            </Spinner><p>This may take a while...</p></div>}
       </Modal.Body>
       {husband.age !== undefined ? <Modal.Footer className='d-flex justify-content-center'>
        <ButtonGroup >
--- src/ModalComponent2.js
+++ src/ModalComponent2.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Modal, Button, ButtonGroup } from 'react-bootstrap'
+import { Modal, Button, ButtonGroup, Spinner } from 'react-bootstrap'
+
 
 function ModalComponent2({ wife, show, closeModal, location, nestedModal, fetchFemalePhoto }) {
   return (
@@ -10,24 +11,26 @@ function ModalComponent2({ wife, show, closeModal, location, nestedModal, fetchF
       aria-labelledby="contained-modal-title-vcenter"
       centered>
       <Modal.Body className="m-4">
-        <h4>I see...</h4>
+        {wife.age !== undefined ? <h4>I see...</h4> : ""}
         {location !== "" && wife.age !== undefined ?
           `Her name is ${wife.age.name}. ` : ""}
         {location !== "" && wife.age !== undefined ?
           `She is ${wife.age.age} years old. ` : ""}
-        {wife.age !== undefined &&location !== "" ?
-          `She is waiting for you in ${location}.` : "This may take a while..."}
+        {wife.age !== undefined && location !== "" ?
+          `She is waiting for you in ${location}.` : <div className="d-flex">
+            <Spinner className="spinner mx-2" animation="border" role="status">
+            </Spinner><p>This may take a while...</p></div>}
       </Modal.Body>
-      <Modal.Footer className='d-flex justify-content-center'>
-      {wife.age !== undefined ? <ButtonGroup >
+      {wife.age !== undefined ? <Modal.Footer className='d-flex justify-content-center'>
+        <ButtonGroup >
           <Button
             onClick={() => { nestedModal(); fetchFemalePhoto(); closeModal() }}
             className="rounded m-3">I'd like to see her.</Button>
           <Button
             className="rounded m-3"
             onClick={closeModal}>Nah, I'll pass.</Button>
-        </ButtonGroup> : ""}
-      </Modal.Footer>
+        </ButtonGroup>
+      </Modal.Footer> : ""}
     </Modal>
   );
 }
